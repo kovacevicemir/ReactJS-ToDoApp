2630b9fb9cdd775d48b695cc28d4cead0582da32
kovacevicemir <emirkovacevic92@gmail.com>
2020-05-15 02:55:59 +1000

    first

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React from "react"
+import TodoItem from "./TodoItem"
+
+const todosData = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Clean gecko tank",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Mow lawn",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Catch up on Arrested Development",
+        completed: false
+    }
+]
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(id) {
+        this.setState(prevState=>{
+            const updatedTodos = prevState.todos.map(todo =>{
+                if(todo.id === id){
+                    return{
+                        ...todo,
+                        completed : !todo.completed
+                    }
+                }
+                return todo
+            })
+
+            console.log(updatedTodos)
+
+            return{
+                todos: updatedTodos
+            }
+        })
+    }
+    
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+        
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )    
+    }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import react and reactdom libs
+// Create a react component
+// take the react comp and show it
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(<App />,document.getElementById('root'))
